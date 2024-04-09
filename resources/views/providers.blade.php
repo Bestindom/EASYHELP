eb52ddd1d886875743d2dfd3ef75f7c611745bd7
Cleo00799 <imartinezl2223@politecnics.barcelona>
2024-04-09 15:54:06 +0200

    adicion de vista para providers sin ruta

---
+++ resources/views/providers.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/css/app.scss'])
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>EASYHELP-Providers</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+            <a class="nav-link active" aria-current="page" href="{{ url('/') }}">AUTOEVALUACIÓN</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    @if (Auth::check() && Auth::user()->rol->tipus == 'Administrador')
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Datos Maestros
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="{{ url('alumne') }}">Admin</a></li>
+                                <li><a class="dropdown-item" href="{{ url('cicle') }}">Profes</a></li>
+                                <li><a class="dropdown-item" href="{{ url('cicle') }}">Alumnos</a></li>
+                                {{-- <li><a class="dropdown-item" href="{{ url('/login')}}">Login</a></li> --}}
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+                <form class="d-flex" role="search">
+                    <ul class="navbar-nav me-2">
+                        @if (Auth::check())
+                            <li class="form-control me-2">
+                                <a class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ Auth::user()->nom }} {{ Auth::user()->cognom }}
+                                </a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="{{ url('/logout') }}">
+                                        <i class="bi bi-box-arrow-left"></i>Logout
+                                    </a>
+                                </div>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link font-weight-bold" href="{{ url('/login') }}">
+                                    <i class="bi bi-box-arrow-in-right"></i> Login
+                                </a>
+                            </li>
+                        @endif
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <form>
+        <select name="" id=""></select>
+    </form>
+    <footer class="bg-primary text-white text-center p-3">
+        <p>EasyHelp - 2024</p>
+    </footer>
+    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+
+</body>
+@vite(['resources/js/app.js'])
+
+</html>
