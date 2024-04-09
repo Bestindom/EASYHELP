e25abd01cf31043f29e874ae4b68d8ebb0e5f540
EnricMart <emartorellv2223@politecnics.barcelona>
2024-04-09 18:09:11 +0200

    csslanding

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css', 'resources/css/app.scss'])
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/csslanding.css') }}">
     <title>EASYHELP</title>
 </head>
 <body>
