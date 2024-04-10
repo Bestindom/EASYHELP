c761c31e56f303ba1d93765e0394bdbd91c0124b
Cleo00799 <imartinezl2223@politecnics.barcelona>
2024-04-10 17:22:27 +0200

    adicion de contenido en la plantilla para el extends en landing

---
+++ resources/views/layouts/principal.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        @yield('title')
+    </title>
+    @vite(['resources/css/app.css', 'resources/css/app.scss', 'resources/js/app.js'])
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/csslanding.css') }}">
+    <link href='https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.css' rel='stylesheet' />
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">EASYHELP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    @if (Auth::check())
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            {{ Auth::user()->username }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="{{ url('/logout') }}"><i class="bi bi-box-arrow-left"></i> Logout</a></li>
+                        </ul>
+                    </li>
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="{{ url('/login') }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+                    </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+    @yield('contenido')
+
+    <script src='https://api.mapbox.com/mapbox-gl-js/v3.2.0/mapbox-gl.js'></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+
+</html>
