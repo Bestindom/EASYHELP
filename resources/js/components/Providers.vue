779624a4ec45c017b36106aec6b4e1590dbf892c
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-04-16 23:23:18 +0200

    crear botones para incrementar

--- resources/js/components/Providers.vue
+++ resources/js/components/Providers.vue
@@ -1,20 +1,41 @@
 <template>
     <div>
         <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">   
+            <div v-for="(item, index) in items" :key="index" class="col">   
                 <div class="card h-100">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">{{ item.title }}</h5>
+                        <p class="card-text">{{ item.menuCount }} menús</p>
+                        <button @click="increment(index)">+</button>
+                        <button @click="decrement(index)">-</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
 <script>
 export default {
+    data() {
+        return {
+            items: [
+                { title: 'Jiajie', menuCount: 0 },
+                { title: 'Pizza', menuCount: 0 }
+            ]
+        };
+    },
+    methods: {
+        increment(index) {
+            this.items[index].menuCount++;
+        },
+        decrement(index) {
+            if (this.items[index].menuCount > 0) {
+                this.items[index].menuCount--;
+            }
+        }
+    }
 }
 </script>
+
 <style></style>
