eccdf1199e46f8d4408a544fac3ed7dced6a494e
Jiajie Chen <jchen2223@politecnics.barcelona>
2024-04-18 15:41:24 +0200

    Update Registro.vue

--- resources/js/components/Registro.vue
+++ resources/js/components/Registro.vue
@@ -1,165 +1,189 @@
 <template>
   <div>
-      <div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">  
-      <h1>PROVIDERS</h1>
-      <form>
-        <div class="mb-3">
-          <input type="text" class="form-control nombre" aria-describedby="emailHelp" placeholder="Introduce nombre de usuario o email">
+    <div id="carouselExampleIndicators" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <!-- <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+          aria-label="Slide 3"></button> -->
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <h1>PROVIDERS</h1>
+          <form>
+            <div class="mb-3">
+              <input type="text" class="form-control nombre" aria-describedby="emailHelp"
+                placeholder="Introduce nombre de usuario o email">
+            </div>
+            <div class="mb-3">
+              <input type="password" class="form-control paswd" placeholder="Contraseña">
+            </div>
+            <div class="mb-3">
+              <input type="number" class="form-control menu" placeholder="Nombre">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control calle" placeholder="Calle">
+            </div>
+            <!-- <div class="mb-3">
+              <input type="number" class="form-control numero" placeholder="Numero">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control provincia" placeholder="Provincia">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control municipio" placeholder="Municipio">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control cp" placeholder="CP">
+            </div> -->
+            <img src="../../../public/img/provider.svg" alt="Provider_Icon">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
         </div>
-        <div class="mb-3">
-          <input type="password" class="form-control paswd" placeholder="Contraseña">
+        <div class="carousel-item">
+          <h1>RIDERS</h1>
+          <form>
+            <div class="mb-3">
+              <input type="text" class="form-control nombre" aria-describedby="emailHelp"
+                placeholder="Introduce nombre de usuario o email">
+            </div>
+            <div class="mb-3">
+              <input type="password" class="form-control paswd" placeholder="Contraseña">
+            </div>
+            <!-- <div class="mb-3">
+              <input type="number" class="form-control menu" placeholder="Menus">
+            </div> -->
+            <img src="../../../public/img/repartidor.png" alt="Rider_Icon">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
         </div>
-        <div class="mb-3">
-          <input type="number" class="form-control menu" placeholder="Menus">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control calle" placeholder="Calle">
-        </div>
-        <div class="mb-3">
-          <input type="number" class="form-control numero" placeholder="Numero">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control provincia" placeholder="Provincia">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control municipio" placeholder="Municipio">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control cp" placeholder="CP">
-        </div>
-        <img src="../../../public/img/provider.svg" alt="Provider_Icon">
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+        <!-- <div class="carousel-item">
+          <h1>COMEDORES</h1>
+          <form>
+            <div class="mb-3">
+              <input type="text" class="form-control nombre" aria-describedby="emailHelp"
+                placeholder="Introduce nombre de usuario o email">
+            </div>
+            <div class="mb-3">
+              <input type="password" class="form-control paswd" placeholder="Contraseña">
+            </div>
+            <div class="mb-3">
+              <input type="number" class="form-control menu" placeholder="Menus">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control calle" placeholder="Calle">
+            </div>
+            <div class="mb-3">
+              <input type="number" class="form-control numero" placeholder="Numero">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control provincia" placeholder="Provincia">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control municipio" placeholder="Municipio">
+            </div>
+            <div class="mb-3">
+              <input type="text" class="form-control cp" placeholder="CP">
+            </div>
+            <img src="../../../public/img/comedor.svg" alt="Comedor_Icon">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div> -->
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-item">
-      <h1>RIDERS</h1>
-      <form>
-        <div class="mb-3">
-          <input type="text" class="form-control nombre" aria-describedby="emailHelp" placeholder="Introduce nombre de usuario o email">
-        </div>
-        <div class="mb-3">
-          <input type="password" class="form-control paswd" placeholder="Contraseña">
-        </div>
-        <div class="mb-3">
-          <input type="number" class="form-control menu" placeholder="Menus">
-        </div>
-        <img src="../../../public/img/repartidor.png" alt="Rider_Icon">
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-    <div class="carousel-item">
-      <h1>COMEDORES</h1>
-      <form>
-        <div class="mb-3">
-          <input type="text" class="form-control nombre" aria-describedby="emailHelp" placeholder="Introduce nombre de usuario o email">
-        </div>
-        <div class="mb-3">
-          <input type="password" class="form-control paswd" placeholder="Contraseña">
-        </div>
-        <div class="mb-3">
-          <input type="number" class="form-control menu" placeholder="Menus">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control calle" placeholder="Calle">
-        </div>
-        <div class="mb-3">
-          <input type="number" class="form-control numero" placeholder="Numero">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control provincia" placeholder="Provincia">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control municipio" placeholder="Municipio">
-        </div>
-        <div class="mb-3">
-          <input type="text" class="form-control cp" placeholder="CP">
-        </div>
-        <img src="../../../public/img/comedor.svg" alt="Comedor_Icon">
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
   </div>
 </template>
 <script>
 export default {
-  
+
 }
 </script>
 <style scoped>
-.carousel-inner{
-width:500px;
-height:850px;
-margin-left:38%;
+.carousel-inner {
+  width: 500px;
+  height: 850px;
+  margin-left: 38%;
 }
-.nombre, .paswd, .menu, .calle, .numero, .provincia, .municipio, .cp{
-width: 100%;
-height: 50px;
-border-radius: 30px;
-background-color: #FFFBE8;
-border: 2px solid #035177;
+
+.nombre,
+.paswd,
+.menu,
+.calle,
+.numero,
+.provincia,
+.municipio,
+.cp {
+  width: 100%;
+  height: 50px;
+  border-radius: 30px;
+  background-color: #FFFBE8;
+  border: 2px solid #035177;
 }
-.btn{
-width: 300px;
-height: 55px;
-border-radius: 30px;
-background-color: #219EBC;
-color: #FFFDFD;
-border: 1px solid #000000;
-font-weight: bold;
-font-family: "Nunito", sans-serif;
-font-optical-sizing: auto;
-font-style: normal;
-box-shadow: 2px 2px 0px 0px #035177;
-display: flex;
-align-items: center;
-justify-content: center;
-margin-left:18%;
+
+.btn {
+  width: 300px;
+  height: 55px;
+  border-radius: 30px;
+  background-color: #219EBC;
+  color: #FFFDFD;
+  border: 1px solid #000000;
+  font-weight: bold;
+  font-family: "Nunito", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  box-shadow: 2px 2px 0px 0px #035177;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 18%;
 }
-.carousel-control-prev, .carousel-control-next {
-position: absolute;
-top: 0;
-bottom: 0;
-z-index: 1;
-display: flex;
-align-items: center;
-justify-content: center;
-width: 15%;
-padding: 0;
-text-align: center;
-border: 0;
-height: 50px;
-width: 30px;
-margin-top: 10%;
-margin-left: 32%;
-margin-right: 24%;
+
+.carousel-control-prev,
+.carousel-control-next {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 15%;
+  padding: 0;
+  text-align: center;
+  border: 0;
+  height: 50px;
+  width: 30px;
+  margin-top: 10%;
+  margin-left: 32%;
+  margin-right: 24%;
 }
-.carousel-control-next-icon,.carousel-control-prev-icon{
-filter:invert(1)grayscale(100); 
+
+.carousel-control-next-icon,
+.carousel-control-prev-icon {
+  filter: invert(1)grayscale(100);
 }
-placeholder{
-font-weight: bold;
+
+placeholder {
+  font-weight: bold;
 }
+
 .carousel-indicators [data-bs-target] {
-background-color: #035177;
+  background-color: #035177;
 }
-img{
-margin-left: 35%;
+
+img {
+  margin-left: 35%;
 }
 </style>
