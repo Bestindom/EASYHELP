d067f8ad9c5232551bdceb968f219c1e969e19b6
Jiajie Chen <jchen2223@politecnics.barcelona>
2024-04-19 16:31:38 +0200

    acabado

--- resources/js/components/Registro.vue
+++ resources/js/components/Registro.vue
@@ -2,22 +2,38 @@
 
   <div class="register">
     <div class="row">
-      <div class="col-6 image">
-        <div class="row background-images">
-          <div class="col-6"></div>
-          <div class="col-6"></div>
-          <div class="col-6"></div>
-          <div class="col-6"></div>
-        </div>
-      </div>
-      <div class="col-6 register-form">
-        <div class="toggle-buttons">
+      <div class="col-6 rider-col">
+        <div class="toggle-buttons" id="riderButtons">
           <div class="buttons">
             <button :class="{ active: activeOption === 'rider' }" @click="changeForm('rider')">Rider</button>
             <button :class="{ active: activeOption === 'provider' }" @click="changeForm('provider')">Proveedor</button>
           </div>
         </div>
+        <div class="container-fluid rider" id="form2">
+          <div class="provider-register-container">
+            <h1>Registro Riders</h1>
+            <form class="rider-form">
+              <div class="mb-3">
+                <input type="text" class="form-control nombre" aria-describedby="emailHelp"
+                  placeholder="Introduce nombre de usuario o email">
+              </div>
+              <div class="mb-3">
+                <input type="password" class="form-control paswd" placeholder="Contraseña">
+              </div>
+              <img src="../../../public/img/repartidor.png" alt="Rider_Icon">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
 
+      <div class="col-6 provider-col">
+        <div class="toggle-buttons" id="providerButtons">
+          <div class="buttons">
+            <button :class="{ active: activeOption === 'rider' }" @click="changeForm('rider')">Rider</button>
+            <button :class="{ active: activeOption === 'provider' }" @click="changeForm('provider')">Proveedor</button>
+          </div>
+        </div>
         <div class="container-fluid provider" id="form1">
           <div class="provider-register-container">
             <h1>Registro Proveedores</h1>
@@ -40,23 +56,6 @@
             </form>
           </div>
         </div>
-
-        <div class="container-fluid rider" id="form2">
-          <div class="provider-register-container">
-            <h1>Registro Riders</h1>
-            <form class="rider-form">
-              <div class="mb-3">
-                <input type="text" class="form-control nombre" aria-describedby="emailHelp"
-                  placeholder="Introduce nombre de usuario o email">
-              </div>
-              <div class="mb-3">
-                <input type="password" class="form-control paswd" placeholder="Contraseña">
-              </div>
-              <img src="../../../public/img/repartidor.png" alt="Rider_Icon">
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -74,16 +73,29 @@ export default {
     changeForm(option) {
       const rider = document.getElementById("form2");
       const provider = document.getElementById("form1");
+      const riderButtons = document.getElementById("riderButtons");
+      const providerButtons = document.getElementById("providerButtons");
+      const riderCol = document.querySelector(".rider-col");
+      const providerCol = document.querySelector(".provider-col");
       this.activeOption = option === 'rider' ? 'rider' : 'provider';
 
       if (this.activeOption === 'rider') {
         rider.style.display = "flex";
         provider.style.display = "none";
+        riderButtons.style.display = "flex";
+        providerButtons.style.display = "none";
+        providerCol.style.backgroundImage ="url(img/registro/rider.webp)";
+        riderCol.style.backgroundImage ="none";
       } else {
         rider.style.display = "none";
         provider.style.display = "flex";
+        riderButtons.style.display = "none";
+        providerButtons.style.display = "flex";
+        riderCol.style.backgroundImage ="url(img/registro/provider.jpg)";
+        providerCol.style.backgroundImage ="none";
       }
-    },
+    }
+    ,
   }
 };
 </script>
@@ -142,17 +154,33 @@ input {
   height: 70vh;
 }
 
-.rider-form {
+.rider-col {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  /* background-image: url(img/registro/provider.jpg); */
+  background-size: cover;
+}
+
+.provider-col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-image: url(img/registro/rider.webp);
+  background-size: cover;
 }
 
 .toggle-buttons {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+#providerButtons,
+#provider-img {
+  display: none;
 }
 
 .buttons {
@@ -178,7 +206,7 @@ input {
   text-align: center;
 }
 
-.row  {
+.row {
   height: 100%;
 }
 
@@ -192,11 +220,5 @@ input {
 
 .image {
   background-color: aquamarine;
-}
-
-.col-6.background-images {
-  background-image: url(img/register.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
 }
 </style>
