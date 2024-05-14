00984f0e48988c32b86b7395dfce11175f48b75f
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-05-14 17:58:40 +0200

    acabado

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -4,7 +4,9 @@
 
 @section('contenido')
         <div id="providers"  >
-        <div></div>
+        <div>
+            
+        </div>
     </div>
     <div id='maps'>
         <div id="map" style="width: 100%; height: 100vh; "></div>
